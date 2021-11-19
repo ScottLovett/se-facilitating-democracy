a94c2a7795317aa842c3fc17f6c26a2413a47857
Scott Lovett <89472636+ScottLovett@users.noreply.github.com>
2021-11-19 16:35:29 -0500

    Create dbAccess.java
    had issues merging, so doing it the clumsy way

---
+++ src/jfx/FacilitatingDemocracy/dbAccess.java
@@ -0,0 +1,48 @@
+package jfx;
+
+import java.sql.*;
+
+public class dbAccess {
+    private final String url = "jdbc:postgresql://localhost/democracy";
+    private final String user = "postgres";
+    private final String password = "postgres";
+
+// attempt to connect with postgres server
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public void getUsers() {
+
+        String SQL = "SELECT Name, studentnum FROM users";
+
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(SQL)) {
+            // display actor information
+            displayUsers(rs);
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private void displayUsers(ResultSet rs) throws SQLException {
+        while (rs.next()) {
+            System.out.println(rs.getString("name") + "\t"
+                    + rs.getString("studentnum"));
+        }
+    }
+
+    public static void main(String[] args) {
+        dbAccess app = new dbAccess();
+        app.getUsers();
+    }
+}
