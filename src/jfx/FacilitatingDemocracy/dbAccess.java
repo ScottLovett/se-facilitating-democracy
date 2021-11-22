044f54ca34eb17a1835f5a850e8f81d3370824a5
Scott Lovett <89472636+ScottLovett@users.noreply.github.com>
2021-11-22 16:28:16 -0500

    Update dbAccess.java
    added get totalvotes for use for the elec threshold for stv
    
    added elimination prototype, going to see if i can make this more generic

--- src/jfx/FacilitatingDemocracy/dbAccess.java
+++ src/jfx/FacilitatingDemocracy/dbAccess.java
@@ -20,25 +20,20 @@ public class dbAccess {
         return conn;
     }
 
-    public void getUsers() {
+    public int getTotalVotes() {
 
-        String SQL = "SELECT name, studentnum FROM users";
-
+        String SQL = "SELECT count(*) FROM votes";
+        int count = 0;
+        
         try (Connection conn = connect();
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(SQL)) {
-            // display user information
-            displayUsers(rs);
+             rs.next();
+             count = rs.getInt(1);
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
-    }
-
-    private void displayUsers(ResultSet rs) throws SQLException {
-        while (rs.next()) {
-            System.out.println(rs.getString("name") + "\t"
-                    + rs.getString("studentnum"));
-        }
+        return count;
     }
 
     public int getFirstVote(int candidatenum) {
@@ -55,13 +50,30 @@ public class dbAccess {
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
+        return count;
+    }
 
+    public int getFirstElimination(int elim, int candidatenum) {
+        String SQL = "SELECT count(*) FROM votes " + "WHERE vote1 = elim1 " + "AND vote2 = candidatenum";
+        int count = 0;
+
+        try (Connection conn = connect();
+             PreparedStatement pstmt = conn.prepareStatement(SQL)) {
+
+            pstmt.setInt(1, elim);
+            pstmt.setInt(2, candidatenum);
+            ResultSet rs = pstmt.executeQuery();
+            count = rs.getInt(2);
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
         return count;
     }
 
 
     public static void main(String[] args) {
         dbAccess app = new dbAccess();
-        app.getUsers();
+        System.out.println(app.getTotalVotes());
     }
 }
